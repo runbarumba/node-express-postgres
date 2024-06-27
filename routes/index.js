9a8745716fe0d3f5b010fa74a0c2498d6525926d
runbarumba <166688257+runbarumba@users.noreply.github.com>
2024-06-27 07:16:59 +0000

    15章途中

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
+const knex = require('../db/knex');
+//const pg = require('pg');
 
-const connection = new pg.Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'postgres',
-  database: 'todo_app',
-  port: 5432,
-  });
+//const connection = new pg.Pool({
+//host: 'localhost',
+//user: 'root',
+//password: 'postgres',
+//database: 'todo_app',
+//port: 5432,
+//});
 
-
+let todos = [];
 
 router.get('/', function (req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
-      console.log(results.rows);
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results.rows,
-      });
-    }
-  );
+knex("tasks")
+.select("*")
+.then(function (results) {
+console.log(results);
+res.render('index', {
+title: 'ToDo App',
+todos: results,
+});
+})
+.catch(function (err) {
+console.error(err);
+res.render('index', {
+title: 'ToDo App',
+});
+});
 });
 
 router.post('/', function (req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
-  const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+const todo = req.body.add;
+knex("tasks")
+.insert({user_id: 1, content: todo})
+.then(function () {
+res.redirect('/')
+})
+.catch(function (err) {
+console.error(err);
+res.render('index', {
+title: 'ToDo App',
 });
+});
+});
+
+router.use('/signup', require('./signup'));
 
 module.exports = router;
