112968d3e4f8a8ae445fd7c009b19e5300a4c05d
runbarumba <166688257+runbarumba@users.noreply.github.com>
2024-07-11 06:34:43 +0000

    chapter15 end

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cookieSession = require("cookie-session");
+const secret = "secretCuisine123";
+
 const app = express();
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [secret],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -2,71 +2,47 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
-    const username = req.body.username;
-    const password = req.body.password;
-    const repassword = req.body.repassword;
-  
-    knex("users")
-      .where({name: username})
-      .select("*")
-      .then(function (result) {
-        if (result.length !== 0) {
-          res.render("signup", {
-            title: "Sign up",
-            errorMessage: ["このユーザ名は既に使われています"],
-          }) 
-        } else if (password === repassword) {
-          knex("users")
-            .insert({name: username, password: password})
-            .then(function () {
-              res.redirect("/");
-            })
-            .catch(function (err) {
-              console.error(err);
-              res.render("signup", {
-                title: "Sign up",
-                errorMessage: [err.sqlMessage],
-              });
-            });
-        } else {
-          res.render("signup", {
-            title: "Sign up",
-            errorMessage: ["パスワードが一致しません"],
-          });
-        }
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render("signup", {
-          title: "Sign up",
-          errorMessage: [err.sqlMessage],
-        });
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
       });
-  });
-router.use('/signup', require('./signup'));
-
-module.exports = router;
+    });
+});
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
