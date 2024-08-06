8f7478df92c3db32c495114116b932178ee0bbf3
runbarumba <166688257+runbarumba@users.noreply.github.com>
2024-08-06 14:58:02 +0000

    kansei

--- routes/signin.js
+++ routes/signin.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   res.render("signin", {
     title: "Sign in",
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
 
   knex("users")
     .where({
       name: username,
-      password: password,
+      
     })
     .select("*")
-    .then((results) => {
+    .then(async function (results) {
+      
       if (results.length === 0) {
         res.render("signin", {
           title: "Sign in",
           errorMessage: ["ユーザが見つかりません"],
+          isAuth: isAuth,
         });
       } else {
         req.session.userid = results[0].id;
@@ -38,6 +42,7 @@ const isAuth = Boolean(userId);
       res.render("signin", {
         title: "Sign in",
         errorMessage: [err.sqlMessage],
+        isAuth: isAuth,
       });
     });
 });
--- routes/signup.js
+++ routes/signup.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const bcrypt = require("bcrypt");
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
 const isAuth = Boolean(userId);
+
+
   res.render('signup', {
     title: 'Sign up',
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -19,15 +23,18 @@ const isAuth = Boolean(userId);
   knex("users")
     .where({name: username})
     .select("*")
-    .then(function (result) {
+    .then(async function (result) {
       if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
           errorMessage: ["このユーザ名は既に使われています"],
-        }) 
+          isAuth: isAuth,
+        })
       } else if (password === repassword) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
         knex("users")
-          .insert({name: username, password: password})
+          .insert({name: username, password: hashedPassword})
           .then(function () {
             res.redirect("/");
           })
@@ -36,12 +43,14 @@ const isAuth = Boolean(userId);
             res.render("signup", {
               title: "Sign up",
               errorMessage: [err.sqlMessage],
+              isAuth: isAuth,
             });
           });
       } else {
         res.render("signup", {
           title: "Sign up",
           errorMessage: ["パスワードが一致しません"],
+          isAuth: isAuth,
         });
       }
     })
@@ -50,6 +59,7 @@ const isAuth = Boolean(userId);
       res.render("signup", {
         title: "Sign up",
         errorMessage: [err.sqlMessage],
+        isAuth: isAuth,
       });
     });
 });
